c397e125890430d42467b62296f99c2ded4c6b6b
NOVER <thamtuducanh12@gmail.com>
2023-10-27 23:32:24 +0700

    1.2: Done Splash Screen and working on SignIn/SignUp

--- app/src/main/java/app/nover/clothingstore/MainActivity.java
+++ app/src/main/java/app/nover/clothingstore/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
