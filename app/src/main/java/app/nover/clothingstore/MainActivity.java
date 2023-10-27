39140b92f7ce8e0dbcba00ef280dcebc094e5173
NOVER <thamtuducanh12@gmail.com>
2023-10-27 21:00:57 +0700

    1.1: Done NavigationBar and working on Home Tab

---
+++ app/src/main/java/app/nover/clothingstore/MainActivity.java
@@ -0,0 +1,52 @@
+package app.nover.clothingstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import app.nover.clothingstore.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_main);
+        replaceFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.home_nav);
+
+        //change Tab in Bottom Navigation Bar
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.menu_nav) {
+                replaceFragment(new MenuFragment());
+            } else if (itemId == R.id.noti_nav) {
+                replaceFragment(new NotificationsFragment());
+            } else if (itemId == R.id.home_nav) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.cart_nav) {
+                replaceFragment(new CartFragment());
+            } else if (itemId == R.id.profile_nav) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment, fragment);
+        fragmentTransaction.commit();
+    }
+}
